3b73e2774635795799d8f2ad7059888a3529f31e
KNB36\USer <ben-orli.nathanson@meduniwien.ac.at>
2018-04-12 17:13:33 +0200

    Name change

--- Dual2AFCBen.m
+++ Dual2AFCBen.m
@@ -1,4 +1,4 @@
-function Dual2AFC
+function Dual2AFCBen
 % 2-AFC olfactory and auditory discrimination task implented for Bpod fork https://github.com/KepecsLab/bpod
 % This project is available on https://github.com/KepecsLab/BpodProtocols_Olf2AFC/
 
