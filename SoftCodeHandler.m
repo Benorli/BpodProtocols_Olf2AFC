05e1586450cf89634bddb83b39f8ac0d57f82bf9
KNB38\USer <ben-orli.nathanson@meduniwien.ac.at>
2018-04-12 16:43:20 +0200

    Reduce volume of sampling error feedback (to prevent distraction in adjacent boxes)

--- SoftCodeHandler.m
+++ SoftCodeHandler.m
@@ -34,14 +34,14 @@ if softCode > 10 && softCode < 21 %for auditory
     if ~BpodSystem.EmulatorMode
         if softCode == 11 %noise on chan 1
             ProgramPulsePal(BpodSystem.Data.Custom.PulsePalParamFeedback);
-            SendCustomPulseTrain(1,cumsum(randi(9,1,601))/10000,(rand(1,601)-.5)*20); % White(?) noise on channel 1+2
-            SendCustomPulseTrain(2,cumsum(randi(9,1,601))/10000,(rand(1,601)-.5)*20);
+            SendCustomPulseTrain(1,cumsum(randi(9,1,601))/10000,(rand(1,601)-.5)*5); % White(?) noise on channel 1+2
+            SendCustomPulseTrain(2,cumsum(randi(9,1,601))/10000,(rand(1,601)-.5)*5);
             TriggerPulsePal(1,2);
             ProgramPulsePal(BpodSystem.Data.Custom.PulsePalParamStimulus);
         elseif softCode == 12 %beep on chan 2
             ProgramPulsePal(BpodSystem.Data.Custom.PulsePalParamFeedback);
-            SendCustomPulseTrain(2,0:.001:.3,(ones(1,301)*3));  % Beep on channel 1+2
-            SendCustomPulseTrain(1,0:.001:.3,(ones(1,301)*3));
+            SendCustomPulseTrain(2,0:.001:.3,(ones(1,301)*1));  % Beep on channel 1+2
+            SendCustomPulseTrain(1,0:.001:.3,(ones(1,301)*1));
             TriggerPulsePal(1,2);
             ProgramPulsePal(BpodSystem.Data.Custom.PulsePalParamStimulus);
         end
